f4372c23371651efae0005b16a06110b7a4fb5c9
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-09-18 14:06:34 +0300

    Fix code style

---
+++ OOP/HWDefiningClassesTask03PCCatalog/Test.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Test.cs" company="Softuni">
+// Copyright (c) 2014 All Rights Reserved
+// <author>Me</author>
+// </copyright>
+      
+namespace HWDefiningClassesTask03PCCatalog
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+        }
+    }
+}
