a6179cabb3c8b0fc0fa7684427a89cb18d2e2e0b
georgi-s <georgi.j.stoyanov@gmail.com>
2014-09-19 01:29:53 +0300

    Code style improvement HWDefiningClassesTask04SULS

--- OOP/HWDefiningClassesTask04SULS/Test.cs
+++ OOP/HWDefiningClassesTask04SULS/Test.cs
@@ -12,50 +12,50 @@ namespace HWDefiningClassesTask04SULS
     {
         public static void Main()
         {
-            JuniorTrainer jt1 = new JuniorTrainer("Pesho", "Peshov", 24);
-            JuniorTrainer jt2 = new JuniorTrainer("Gosho", "Goshov", 27);
-            JuniorTrainer jt3 = new JuniorTrainer("Vladko", "Vladkov", 19);
-            SeniorTrainer st1 = new SeniorTrainer("Petko", "Petkov", 33);
-            SeniorTrainer st2 = new SeniorTrainer("Stefan", "Stefanov", 38);
-            SeniorTrainer st3 = new SeniorTrainer("Martin", "Martinov", 29);
-            GraduateStudent gt1 = new GraduateStudent("Ivan", "Novakov", 35, 30020925, 4.45M);
-            GraduateStudent gt2 = new GraduateStudent("Radoslav", "Simeonov", 28, 30058925, 5.80M);
-            GraduateStudent gt3 = new GraduateStudent("Teodor", "Stoychev", 33, 300521345, 3.00M);
-            DropoutStudent do1 = new DropoutStudent("Nakovalnq", "Nakovalnev", 35, 30020925, 4.45M, "...");
-            DropoutStudent do2 = new DropoutStudent("Mmmm", "IdropOut", 28, 30058925, 5.80M, "I don't care zzzZZZzzz...");
-            DropoutStudent do3 = new DropoutStudent("Arnold", "Ill' be back", 33, 300521345, 3.00M, "Terminated!!!");
-            OnlineStudent ost1 = new OnlineStudent("Emil", "Stefanov", 48, 40018512, 3.33M, "OOP");
-            OnlineStudent ost2 = new OnlineStudent("Martin", "Todorov", 22, 40065415, 5.45M, "OOP");
-            OnlineStudent ost3 = new OnlineStudent("Valeri", "Zahariev", 18, 40018525, 4.89M, "OOP");
-            OnsiteStudent os1 = new OnsiteStudent("Natalya", "Alexandrova", 31, 40089564, 4.00M, "OOP", 5);
-            OnsiteStudent os2 = new OnsiteStudent("Adriyana", "Alexandrova", 25, 40012188, 5.25M, "OOP", 5);
-            OnsiteStudent os3 = new OnsiteStudent("Peter", "Simeonov", 30, 40035698, 3.80M, "OOP", 3);
+            JuniorTrainer juniorTrainer1 = new JuniorTrainer("Pesho", "Peshov", 24);
+            JuniorTrainer juniorTrainer2 = new JuniorTrainer("GonlineStudentho", "GonlineStudenthov", 27);
+            JuniorTrainer juniorTrainer3 = new JuniorTrainer("Vladko", "Vladkov", 19);
+            SeniorTrainer seniorTrainer1 = new SeniorTrainer("Petko", "Petkov", 33);
+            SeniorTrainer seniorTrainer2 = new SeniorTrainer("Stefan", "Stefanov", 38);
+            SeniorTrainer seniorTrainer3 = new SeniorTrainer("Martin", "Martinov", 29);
+            GraduateStudent graduateStudent1 = new GraduateStudent("Ivan", "Novakov", 35, 30020925, 4.45M);
+            GraduateStudent graduateStudent2 = new GraduateStudent("RadonlineStudentlav", "Simeonov", 28, 30058925, 5.80M);
+            GraduateStudent graduateStudent3 = new GraduateStudent("Teodor", "Stoychev", 33, 300521345, 3.00M);
+            DropoutStudent dropoutStudent1 = new DropoutStudent("Nakovalnq", "Nakovalnev", 35, 30020925, 4.45M, "...");
+            DropoutStudent dropoutStudent2 = new DropoutStudent("Mmmm", "IdropOut", 28, 30058925, 5.80M, "I dropoutStudentn't care zzzZZZzzz...");
+            DropoutStudent dropoutStudent3 = new DropoutStudent("Arnold", "Ill' be back", 33, 300521345, 3.00M, "Terminated!!!");
+            OnlineStudent onlineStudent1 = new OnlineStudent("Emil", "Stefanov", 48, 40018512, 3.33M, "OOP");
+            OnlineStudent onlineStudent2 = new OnlineStudent("Martin", "Todorov", 22, 40065415, 5.45M, "OOP");
+            OnlineStudent onlineStudent3 = new OnlineStudent("Valeri", "Zahariev", 18, 40018525, 4.89M, "OOP");
+            OnsiteStudent onsiteStudent1 = new OnsiteStudent("Natalya", "Alexandrova", 31, 40089564, 4.00M, "OOP", 5);
+            OnsiteStudent onsiteStudent2 = new OnsiteStudent("Adriyana", "Alexandrova", 25, 40012188, 5.25M, "OOP", 5);
+            OnsiteStudent onsiteStudent3 = new OnsiteStudent("Peter", "Simeonov", 30, 40035698, 3.80M, "OOP", 3);
 
-            jt1.CreateCourse("Drink brave!");
-            st1.CreateCourse("I'm alive!");
-            st2.DeleteCourse("Drink brave!");
-            do3.Reapply();
+            juniorTrainer1.CreateCourse("Drink brave!");
+           seniorTrainer1.CreateCourse("I'm alive!");
+           seniorTrainer2.DeleteCourse("Drink brave!");
+            dropoutStudent3.Reapply();
 
             List<Person> softUniPersons = new List<Person> 
             { 
-                jt1,
-                jt2,
-                jt3,
-                st1,
-                st2,
-                st3,
-                gt1,
-                gt2,
-                gt3,
-                do1,
-                do2,
-                do3,
-                ost1,
-                ost2,
-                ost3,
-                os1,
-                os2,
-                os3 
+                juniorTrainer1,
+                juniorTrainer2,
+                juniorTrainer3,
+                seniorTrainer1,
+                seniorTrainer2,
+                seniorTrainer3,
+                graduateStudent1,
+                graduateStudent2,
+                graduateStudent3,
+                dropoutStudent1,
+                dropoutStudent2,
+                dropoutStudent3,
+                onsiteStudent1,
+                onsiteStudent2,
+                onsiteStudent3,
+                onlineStudent1,
+                onlineStudent2,
+                onlineStudent3 
             };
 
             SULSTest testSoftUni = new SULSTest(softUniPersons);
