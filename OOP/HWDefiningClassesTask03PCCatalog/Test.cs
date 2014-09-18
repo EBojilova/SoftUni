38eea5297bba2818bf198cfe30f425fefee44d3c
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-09-18 17:11:22 +0300

    Added comments

--- OOP/HWDefiningClassesTask03PCCatalog/Test.cs
+++ OOP/HWDefiningClassesTask03PCCatalog/Test.cs
@@ -12,6 +12,7 @@ namespace HWDefiningClassesTask03PCCatalog
     {
         public static void Main()
         {
+            // Test Component constructors
             Component component1 = new Component("Motherboard", "Asus", 200M);
             Component component2 = new Component("Processor", "Intel i7 4x2Ghz", 250M);
             Component component3 = new Component("RAM", "Kingston 2 X 4GB", 150M);
@@ -26,35 +27,45 @@ namespace HWDefiningClassesTask03PCCatalog
                 component1, component2, component3, component4, component5, component6, component7, component8 
             };
 
+            // Test Computer constructors and My methods :)
             Computer computer3 = new Computer("Stantec3");
-            computer3.AddComponent(component1);
-            component1.Price = 220M;
-            computer3.AddComponent(component2);
+            computer3.AddComponent(component1); // Test my methods :) - adding
+            computer3.AddComponent(component2); // ...
             computer3.AddComponent(component4);
             computer3.AddComponent(component7);
-            computer3.RemoveComponentByName("Keyboard");
+            computer3.RemoveComponentByName("Keyboard");  // Test my methods :) - removing by name
 
             Computer computer2 = new Computer("Stantec2");
             computer2.AddComponent(component1);
             computer2.AddComponent(component2);
             computer2.AddComponent(component3);
-            computer2.RemoveAllComponents();
+            computer2.RemoveAllComponents(); // Test my methods :) - clear all
             computer2.AddComponent(component3);
             computer2.AddComponent(component5);
             computer2.AddComponent(component6);
             computer2.AddComponent(component7);
             computer2.AddComponent(component8);
+            computer2.AddComponent(component1);
 
             Computer computer1 = new Computer("Stantec1", components);
 
+            // Testing reference of components - just for test ;)
+            component1.Price = 220M; // Now in all computers this part have a new value
+            
+
+            // Creating list with computers
             List<Computer> computers = new List<Computer> { computer1, computer2, computer3 };
+
+            // Ordering by price ;) - see Computer class
             computers.Sort();
             
+            // Testing Computer info/description method
             foreach (var computer in computers)
             {
                 Console.WriteLine(computer.GetDescription());
             }
 
+            // Testing Computer ToString() method
             foreach (var computer in computers)
             {
                 Console.WriteLine(computer);
