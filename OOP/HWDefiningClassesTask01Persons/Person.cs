553334f58bd546596d9b0207d55e15556b95f2eb
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-09-18 10:37:37 +0300

    Initial commit 2 :)

---
+++ OOP/HWDefiningClassesTask01Persons/Person.cs
@@ -0,0 +1,117 @@
+﻿// <copyright file="Person.cs" company="Softuni">
+// Copyright (c) 2014 All Rights Reserved
+// <author>Me</author>
+// </copyright>
+
+namespace HWDefiningClassesTask01Persons
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private byte age;
+        private string mail;
+
+        public Person(string name, byte age, string mail)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Mail = mail;
+        }
+
+        public Person(string name, byte age)
+            : this(name, age, null)
+        {
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name", "Name must be non empty or null!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public byte Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Age", "Age must be between 1 and 100!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Mail
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.mail))
+                {
+                    return "N/A";
+                }
+
+                return this.mail;
+            }
+
+            set
+            {
+                if (value != null && !value.Contains("@"))
+                {
+                    throw new ArgumentException("Mail cannot be empty string and must be valid email or null!", "Mail");
+                }
+
+                this.mail = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string name = string.Format("Name: {0}", this.Name);
+            string age = string.Format(" Age: {0}", this.Age);
+            string mail = string.Format("Mail: {0}", this.Mail);
+            int maxLength = this.GetMaxLength(new string[] { name, age, mail });
+            sb.AppendLine(string.Format("{0}{1}{2}", '\u2554', new string('\u2550', maxLength), '\u2557'));
+            sb.AppendLine(string.Format("{0}{1}{0}", '\u2551', name.PadRight(maxLength, ' ')));
+            sb.AppendLine(string.Format("{0}{1}{0}", '\u2551', age.PadRight(maxLength, ' ')));
+            sb.AppendLine(string.Format("{0}{1}{0}", '\u2551', mail.PadRight(maxLength, ' ')));
+            sb.AppendLine(string.Format("{0}{1}{2}", '\u255A', new string('\u2550', maxLength), '\u255D'));
+            return sb.ToString();
+        }
+
+        private int GetMaxLength(string[] strings)
+        {
+            int maxLength = 0;
+            foreach (var str in strings)
+            {
+                if (maxLength < str.Length)
+                {
+                    maxLength = str.Length;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ OOP/HWDefiningClassesTask01Persons/Test.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="Test.cs" company="Softuni">
+// Copyright (c) 2014 All Rights Reserved
+// <author>Me</author>
+// </copyright>
+
+namespace HWDefiningClassesTask01Persons
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            Person[] persons = new Person[2];
+
+            // Test constructor
+            // persons[0] = new Person(); //not compile
+            persons[0] = new Person("Pesho", 21, "peshoviq@mail.com");
+            persons[1] = new Person("Gosho", 43);
+
+            // Test ToString()
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+
+            // Test name validation:
+            // persons[0].Name = ""; //throw exception
+            // persons[0].Name = null; //throw exception
+            persons[0].Name = "New Pesho";
+
+            // Test age validation
+            // persons[0].Age = 0; //throw exception
+            // persons[0].Age = 101; //throw exception
+            persons[0].Age = 1;
+            persons[0].Age = 100;
+
+            // Test mail validation
+            // persons[0].Mail = ""; //throw exception
+            // persons[0].Mail = "invalid.mail.com"; //throw exception
+            persons[0].Mail = null;
+            persons[0].Mail = "valid@mail.com";
+        }
+    }
+}
